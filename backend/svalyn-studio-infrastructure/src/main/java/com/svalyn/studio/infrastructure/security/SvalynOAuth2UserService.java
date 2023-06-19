4af982b3946d7eabb1e8c8da4740428e2056dc77
Stéphane Bégaudeau <stephane.begaudeau@gmail.com>
2023-06-19 21:42:24 +0200

    [101] Stop fetching new OAuth metadata during each login
    Bug: https://github.com/svalyn/svalyn-studio/issues/101
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@gmail.com>

--- backend/svalyn-studio-infrastructure/src/main/java/com/svalyn/studio/infrastructure/security/SvalynOAuth2UserService.java
+++ backend/svalyn-studio-infrastructure/src/main/java/com/svalyn/studio/infrastructure/security/SvalynOAuth2UserService.java
@@ -65,50 +65,50 @@ public class SvalynOAuth2UserService extends DefaultOAuth2UserService {
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
-        this.logger.debug("Data retrieved for user " + userRequest.getClientRegistration().getRegistrationId() + "#" + oAuth2User.getAttributes().get("id"));
+        var provider = userRequest.getClientRegistration().getRegistrationId();
+        var providerId = Optional.ofNullable(oAuth2User.getAttributes().get("id")).map(Object::toString).orElse("");
 
-        OAuth2AuthorizedClient client = new OAuth2AuthorizedClient(userRequest.getClientRegistration(), oAuth2User.getName(), userRequest.getAccessToken());
-        List<Map<String, Object>> additionalData = new ArrayList<>();
+        this.logger.debug("Data retrieved for user {}#{}", provider, providerId);
 
-        if (userRequest.getClientRegistration().getRegistrationId().equalsIgnoreCase("github")) {
-            additionalData = webClient.get()
-                    .uri("https://api.github.com/user/emails")
-                    .attributes(ServletOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient(client))
-                    .retrieve()
-                    .bodyToMono(List.class)
-                    .block();
-        }
-
-        var optionalOAuth2UserInfo = this.getUserInfo(userRequest.getClientRegistration().getRegistrationId(), oAuth2User, additionalData);
-
-        if (optionalOAuth2UserInfo.isEmpty()) {
-            this.logger.debug("OAuth2UserInfo empty");
-            throw new OAuth2AuthenticationException("The OAuth2 provider " + userRequest.getClientRegistration().getRegistrationId() + ", used for the authentication, is not supported");
-        }
-
-        var oAuth2UserInfo = optionalOAuth2UserInfo.get();
         Account account;
 
-        var optionalExistingAccountEntity = this.accountRepository.findByEmail(oAuth2UserInfo.getEmail());
+        var optionalExistingAccountEntity = this.accountRepository.findByOAuth2Metadata(provider, providerId);
         if (optionalExistingAccountEntity.isPresent()) {
             var existingAccountEntity = optionalExistingAccountEntity.get();
+
+            var oAuth2UserInfo = this.getUserInfo(userRequest.getClientRegistration().getRegistrationId(), oAuth2User, List.of());
             account = this.updateAccount(existingAccountEntity, userRequest, oAuth2UserInfo);
         } else {
+            OAuth2AuthorizedClient client = new OAuth2AuthorizedClient(userRequest.getClientRegistration(), oAuth2User.getName(), userRequest.getAccessToken());
+            List<Map<String, Object>> additionalData = new ArrayList<>();
+
+            if (userRequest.getClientRegistration().getRegistrationId().equalsIgnoreCase("github")) {
+                additionalData = webClient.get()
+                        .uri("https://api.github.com/user/emails")
+                        .attributes(ServletOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient(client))
+                        .retrieve()
+                        .bodyToMono(List.class)
+                        .block();
+            }
+
+            var oAuth2UserInfo = this.getUserInfo(userRequest.getClientRegistration().getRegistrationId(), oAuth2User, additionalData);
             account = this.createAccount(userRequest, oAuth2UserInfo);
         }
 
         return new SvalynOAuth2User(account, oAuth2User, userRequest.getAccessToken());
     }
 
-    private Optional<OAuth2UserInfo> getUserInfo(String registrationId, OAuth2User oAuth2User, List<Map<String, Object>> additionalData) {
+    private OAuth2UserInfo getUserInfo(String registrationId, OAuth2User oAuth2User, List<Map<String, Object>> additionalData) {
         if (registrationId.equalsIgnoreCase("github")) {
-            return Optional.of(new GithubOAuth2UserInfo(oAuth2User.getAttributes(), additionalData));
+            return new GithubOAuth2UserInfo(oAuth2User.getAttributes(), additionalData);
         }
-        return Optional.empty();
+
+        this.logger.debug("OAuth2UserInfo empty");
+        throw new OAuth2AuthenticationException("The OAuth2 provider " + registrationId + ", used for the authentication, is not supported");
     }
 
     private Account updateAccount(Account account, OAuth2UserRequest oAuth2UserRequest, OAuth2UserInfo oAuth2UserInfo) {
-        this.logger.debug("Updating account " + oAuth2UserRequest.getClientRegistration().getRegistrationId() + "#" + oAuth2UserInfo.getId());
+        this.logger.debug("Updating account {}#{}", oAuth2UserRequest.getClientRegistration().getRegistrationId(), oAuth2UserInfo.getId());
 
         var hasLoggedWithProvider = account.getOAuth2Metadata().stream()
                 .anyMatch(oAuth2Metadata -> oAuth2Metadata.getProvider().equalsIgnoreCase(oAuth2UserRequest.getClientRegistration().getRegistrationId()));
@@ -127,7 +127,7 @@ public class SvalynOAuth2UserService extends DefaultOAuth2UserService {
     }
 
     private Account createAccount(OAuth2UserRequest oAuth2UserRequest, OAuth2UserInfo oAuth2UserInfo) {
-        this.logger.debug("Creating account " + oAuth2UserRequest.getClientRegistration().getRegistrationId() + "#" + oAuth2UserInfo.getId());
+        this.logger.debug("Creating account {}#{}", oAuth2UserRequest.getClientRegistration().getRegistrationId(), oAuth2UserInfo.getId());
 
         var oAuth2Metadata = OAuth2Metadata.newOAuth2Metadata()
                 .provider(oAuth2UserRequest.getClientRegistration().getRegistrationId())
